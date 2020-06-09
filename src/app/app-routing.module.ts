aaabad449080ef8fb2851ca31e3f9bc3d4631a8f
Mauricio Fraga Jr <mauriciofragajr@gmail.com>
2020-06-09 01:10:08 -0300

    setando fullscreen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ const routes: Routes = [
   {
     path: '', pathMatch: 'full', redirectTo: 'home'
   },
-  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard]},
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
   { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule), canActivate: [AuthGuard] },
   { path: 'facial-rekognition', loadChildren: () => import('./pages/facial-rekognition/facial-rekognition.module').then(m => m.FacialRekognitionModule), canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' }
